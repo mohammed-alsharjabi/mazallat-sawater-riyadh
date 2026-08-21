2c865e3272d36e3909a254f54110dfdc3acf2bc9
mohammed-alsharjabi <mohamedalshrjabi@gmail.com>
2026-08-22 01:54:46 +0300

    feat: publish updated brand logo

--- resources/views/components/site-logo.blade.php
+++ resources/views/components/site-logo.blade.php
@@ -1,8 +1,14 @@
 @props(['compact' => false, 'alt' => 'مظلات وسواتر الرياض'])
 
+@php
+    $logoPath = $compact ? 'brand/mazallat-sawater-riyadh-icon.svg' : 'brand/mazallat-sawater-riyadh-logo.png';
+    $logoFile = public_path($logoPath);
+    $logoVersion = is_file($logoFile) ? filemtime($logoFile) : null;
+@endphp
+
 <img
-    src="{{ asset($compact ? 'brand/mazallat-sawater-riyadh-icon.svg' : 'brand/mazallat-sawater-riyadh-logo.png') }}"
+    src="{{ asset($logoPath) }}{{ $logoVersion ? '?v='.$logoVersion : '' }}"
     alt="{{ $alt }}"
-    @unless($compact) width="960" height="320" @endunless
+    @unless($compact) width="2137" height="736" @endunless
     {{ $attributes->class($compact ? 'site-logo site-logo-compact' : 'site-logo') }}
 >
